ed719d79502b079e4a1b9e086cfbc7c6b8eac42e
LeonardoJaques <jaques.projetos@outlook.com>
2020-11-30 18:49:15 -0300

    Create mult-database, implenent desging pattern strategy, create postgres operatos and connection, instalend sequilize and make first test

---
+++ 08-mult-db -postgress/src/db/strategies/postgres.js
@@ -0,0 +1,69 @@
+const ICrud = require('./../strategies/interfaces/interfaceCrud');
+const Sequelize = require('sequelize');
+
+class Postgres extends ICrud {
+  constructor() {
+    super();
+    this._driver = null;
+    this._herois = null;
+    this._connect();
+  }
+
+  async isConnected() {
+    try {
+      await this._driver.authenticate();
+      return true;
+    } catch (error) {
+      console.error('Deu Ruim!!!', error);
+      return false;
+    }
+  }
+
+  _connect() {
+    this._driver = new Sequelize(
+      'heroes',
+      'leonardjaques01',
+      'minhasenhasecreta',
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        quoteIndenfiers: false,
+        operatorsAliases: 0,
+      }
+    );
+  }
+
+  async defineModel() {
+    this._herois = driver.define(
+      'herois',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          required: true,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        nome: {
+          type: Sequelize.STRING,
+          required: true,
+        },
+        poder: {
+          type: Sequelize.STRING,
+          required: true,
+        },
+      },
+      {
+        tableName: 'tb_herois',
+        freezeTablename: false,
+        timestamps: false,
+      }
+    );
+    await Herois.sync();
+  }
+
+  create(item) {
+    console.log('Item salvo no postgres');
+  }
+}
+
+module.exports = Postgres;
