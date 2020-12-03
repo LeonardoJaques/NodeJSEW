9ec60f7cc4b60230238e376e279db1ca49ebb811
LeonardoJaques <jaques.projetos@outlook.com>
2020-12-02 21:18:28 -0300

    teste http vanilha

---
+++ 12-modulo-http/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http
+  .createServer((request, response) => {
+    response.end('Hello Node!!!');
+  })
+  .listen(5000, () => {
+    console.log('Servidor está rodando!!');
+  });
