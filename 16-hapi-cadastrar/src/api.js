fa5157ca13b50681ca9cdd9d27d95d8c26f0ee75
LeonardoJaques <jaques.projetos@outlook.com>
2020-12-03 19:04:07 -0300

    register heroes with post

---
+++ 16-hapi-cadastrar/src/api.js
@@ -0,0 +1,26 @@
+const Hapi = require('@hapi/hapi');
+const Joi = require('joi');
+const Context = require('./db/strategies/base/contextStrategy');
+const MongoDB = require('./db/strategies/mongodb/mongodb');
+const HeroisSchema = require('./db/strategies/mongodb/schemas/heroisSchemas');
+const HeroRoute = require('./routes/heroRoutes');
+
+const app = Hapi.Server({ port: 5000, host: 'localhost' });
+app.validator(Joi);
+
+function mapRoutes(instance, methods) {
+  return methods.map((methods) => instance[methods]());
+}
+
+async function main() {
+  const connection = MongoDB.connect();
+  const context = new Context(new MongoDB(connection, HeroisSchema));
+
+  app.route([...mapRoutes(new HeroRoute(context), HeroRoute.methods())]);
+
+  await app.start();
+  console.log('Servidor rodando na porta', app.info.port);
+  return app;
+}
+
+module.exports = main();
